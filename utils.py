bd2b196004d597acc0db20bee4b3999a3c7bac4f
49601000 <the3stigmataofpalmereldritch@gmail.com>
2025-07-31 16:24:32 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ def calculate_yield_avg_1y(symbols):
     etf = yf.Ticker(symbols)
     dividends = etf.dividends
 
+    dividends.index = pd.to_datetime(dividends.index)
     one_year_ago = pd.Timestamp.today() - pd.DateOffset(years=1)
     recent_dividends = dividends[dividends.index >= one_year_ago]
 
