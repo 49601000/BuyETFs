ed34cd4ced8a4fba5a91a586dea6b5b53bc18f16
49601000 <the3stigmataofpalmereldritch@gmail.com>
2025-07-31 16:10:37 +0900

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,18 @@
+# utils.py
+import yfinance as yf
+import pandas as pd
+
+def calculate_yield_avg_1y(symbol):
+    etf = yf.Ticker(symbol)
+    dividends = etf.dividends
+
+    one_year_ago = pd.Timestamp.today() - pd.DateOffset(years=1)
+    recent_dividends = dividends[dividends.index >= one_year_ago]
+
+    if recent_dividends.empty:
+        return None
+
+    total_dividends = recent_dividends.sum()
+    current_price = etf.history(period="1d")["Close"].iloc[0]
+
+    return (total_dividends / current_price) * 100
